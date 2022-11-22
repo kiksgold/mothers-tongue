2a8a81f39ca76aa24792a0af4bcd195ba5cb0cab
kiksgold <kiksgold@gmail.com>
2022-11-22 22:40:01 +0000

    Define function for category view

--- blog/views.py
+++ blog/views.py
@@ -32,6 +32,13 @@ class PostDetail(View):
         )
 
 
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    posts = category.posts.filter(status=Post.ACTIVE)
+
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+
 def search(request):
     # Define a function for search query
     query = request.GET.get('query', '')
--- motherstongue/urls.py
+++ motherstongue/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+
+from blog.sitemaps import CategorySitemap, PostSitemap
+
+sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog-urls'),
